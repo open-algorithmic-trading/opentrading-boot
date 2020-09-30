48e44b5c5a79c31f7e620a9a9b8d2d85990a8231
bh4r4th <bh4r4th@gmail.com>
2020-09-30 04:18:57 -0700

    gradle project

---
+++ src/main/java/dev/opentrading/core/OpentradingCoreApplication.java
@@ -0,0 +1,13 @@
+package dev.opentrading.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpentradingCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpentradingCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/opentrading/core/OpentradingCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.opentrading.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpentradingCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
