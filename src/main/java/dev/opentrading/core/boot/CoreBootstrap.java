e1e0b97d354eb95b04d0762a251fefd4066cd2bb
bh4r4th <bh4r4th@gmail.com>
2020-10-05 03:32:38 -0700

    bootstrap, properties

--- src/main/java/dev/opentrading/core/boot/CoreBootstrap.java
+++ src/main/java/dev/opentrading/core/boot/CoreBootstrap.java
@@ -1,68 +1,71 @@
 package dev.opentrading.core.boot;
 
+import javax.inject.Named;
+
 /**
  * Core Bootstrap interface for OpenTrading platform.
  * Broker specific module implements each operations.
  */
+@Named
 public interface CoreBootstrap {
 
-    /***
+    /**
      * Logs into broker trading account.
      * @param <T> Return type could be as simple as a Boolean.
      * @return returns operation status.
      */
     <T> T login();
 
-    /***
+    /**
      * Fetches account margin.
      * @param <T> Return type could be as simple as a Boolean.
      * @return returns operation status.
      */
-    <T> T margin();
+    <T> T getMargin();
 
-    /***
+    /**
      * Loads all instruments for defined configuration in application properties.
      * @param <T> Return type could be as simple as a Boolean.
      * @return returns operation status.
      */
-    <T> T instruments();
+    <T> T getAllInstruments();
 
-    /***
+    /**
      * Filters trading instruments.
      * @param <T> Return type could be as simple as a Boolean.
      * @return returns operation status.
      */
-    <T> T tradingInstruments();
+    <T> T getTradingInstruments();
 
-    /***
+    /**
      * Initializes WebSocket.
      * @param <T> Return type could be as simple as a Boolean.
      * @return returns operation status.
      */
-    <T> T initWebsocket();
+    <T> T initWebSocket();
 
-    /***
+    /**
      * Streams ticks of stocks over WebSocket.
      * @param <T> Return type could be as simple as a Boolean.
      * @return returns operation status.
      */
     <T> T streamTicks();
 
-    /***
+    /**
      * Schedules reading ticks of stocks at regular intervals.
      * @param <T> Return type could be as simple as a Boolean.
      * @return returns operation status.
      */
     <T> T scheduleTicksReading();
 
-    /***
+    /**
      * Schedules ticks aggregator (bars/candlesticks)
      * @param <T> Return type could be as simple as a Boolean.
      * @return returns operation status.
      */
     <T> T scheduleTicksAggregator();
 
-    /***
+    /**
      * Initializes market data simulator.
      * @param <T> Return type could be as simple as a Boolean.
      * @return returns operation status.
--- src/main/java/dev/opentrading/core/spring/CoreApplicationRunner.java
+++ src/main/java/dev/opentrading/core/spring/CoreApplicationRunner.java
@@ -1,5 +1,6 @@
 package dev.opentrading.core.spring;
 
+import dev.opentrading.core.boot.CoreBootstrap;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -17,13 +18,43 @@ public abstract class CoreApplicationRunner implements ApplicationRunner {
 
     private final CoreApplicationProperties applicationProperties;
 
-    protected CoreApplicationRunner(CoreApplicationProperties applicationProperties) {
+    private final CoreBootstrap coreBootstrap;
+
+    protected CoreApplicationRunner(CoreApplicationProperties applicationProperties, CoreBootstrap coreBootstrap) {
         this.applicationProperties = applicationProperties;
+        this.coreBootstrap = coreBootstrap;
     }
 
     @Override
     public void run(ApplicationArguments args) {
-        log.info("CoreApplicationRunner(1) started");
+        log.info("CoreApplicationRunner(1) starting...");
+        if (coreBootstrap.login()) {
+            log.info("Logged in");
+            if (coreBootstrap.getMargin()) {
+                log.info("Retrieved margin");
+                if (coreBootstrap.getAllInstruments()) {
+                    log.info("Loaded all instruments");
+                    if (coreBootstrap.getTradingInstruments()) {
+                        log.info("Filtered trading instruments");
+                        if (coreBootstrap.initSimulator()) {
+
+                        }
+                        if (coreBootstrap.scheduleTicksReading()) {
+                            log.info("Scheduled ticksReading");
+                            if (coreBootstrap.initWebSocket()) {
+                                log.info("Initialized WebSocket");
+                                if (coreBootstrap.streamTicks()) {
+                                    log.info("Started streaming ticks");
+                                    if (coreBootstrap.scheduleTicksAggregator()) {
+                                        log.info("Scheduled ticks aggregator");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
 }
